921542f992360e06388d756e471886d9e1c662a3
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2021-03-23 10:40:27 +0100

    Image Presentation Service: add viewport tile

--- core/src/view/AbstractView_EventConsumption.cpp
+++ core/src/view/AbstractView_EventConsumption.cpp
@@ -103,7 +103,8 @@ std::vector<std::string> get_gl_view_runtime_resources_requests() {
 bool view_rendering_execution(
     void* module_ptr,
     std::vector<megamol::frontend::FrontendResource> const& resources,
-    megamol::frontend_resources::GenericImage& frontend_image)
+    megamol::frontend_resources::GenericImage& frontend_image,
+    ViewportTile viewport_tile/*unused*/)
 {
     megamol::core::view::AbstractView* view_ptr =
         dynamic_cast<megamol::core::view::AbstractView*>(static_cast<megamol::core::Module*>(module_ptr));
@@ -128,7 +129,8 @@ bool view_rendering_execution(
 bool view_init_rendering_state(
     void* module_ptr,
     std::vector<megamol::frontend::FrontendResource> const& resources,
-    megamol::frontend_resources::GenericImage& frontend_image)
+    megamol::frontend_resources::GenericImage& frontend_image,
+    ViewportTile viewport_tile)
 {
     megamol::core::view::AbstractView* view_ptr =
         dynamic_cast<megamol::core::view::AbstractView*>(static_cast<megamol::core::Module*>(module_ptr));
@@ -140,6 +142,7 @@ bool view_init_rendering_state(
     
     megamol::core::view::AbstractView& view = *view_ptr;
 
+    // TODO: view.SetViewportTile(viewport_tile);
     frontend_image = view.InitGenericImageWithImageType();
 
     // fake resize events for view to consume
